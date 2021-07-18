cc814daf401090fa7cf7af0ae1a3069ff2ae83dd
Steve Carvajal <exactas1999@gmail.com>
2021-07-17 20:49:54 -0500

    Aplicacion de mensajeria con Firebase y Angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAo5s90-lrP9HB59-J9_1fo9kiEKNVHVTo",
+    authDomain: "firechat-4bc51.firebaseapp.com",
+    projectId: "firechat-4bc51",
+    storageBucket: "firechat-4bc51.appspot.com",
+    messagingSenderId: "564387736579",
+    appId: "1:564387736579:web:8c250a2bde8ab2beb3b236"
+  }
 };
 
 /*
